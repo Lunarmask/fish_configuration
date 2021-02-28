6945ed86ff6a5cc93d61046c2027c167c6558608
Lunarmask_ <caleb.broyles@tuta.io>
2021-02-28 12:08:23 -0600

    master Re-wrote functions/extractor.fish
    Possibly over-enginnered the extractor.fish function.
    It now parses the path and name of the file being uncompressed.
    
    Performing filename regex matching wasn't always working properly.
    extractor.fish will look at the last two parts of the filename extension
    to see which extracting binary to use.
    
    - Added an argument check for file reachability
    - Using 7z will output to a specified folder

--- functions/extractor.fish
+++ functions/extractor.fish
@@ -12,37 +12,59 @@ function extractor
     return 1
   end
 
-  switch $argv
-    case *.7z
-      7z x $argv
-    case *.zip
-      unzip $argv
-    case *.tar.bz2
-      tar xjf $argv
-    case *.tbz2
-      tar xjf $argv
-    case *.tar.gz
-      tar xzf $argv
-    case *.bz2
-      bunzip2 $argv
-    case *.rar
-      unrar x $argv
-    case *.gz
-      gunzip $argv
-    case *.tar
-      tar xf $argv
-    case *.tgz
-      tar xzf $argv
-    case *.Z
-      uncompress $1;;
-    case *.deb
-      ar x $argv
-    case *.tar.xz
-      tar xf $argv
-    case *.tar.zst
-      unzstd $argv
-    case *
-      echo "'$argv' cannot be extracted via extractor"
+  if test -e $argv
+    echo -e "\nExtracting $argv\n"
+  else
+    echo "The file doesn't exist or isn't reachable"
+    return 1
+  end
+
+  set arg_parts (basename $argv | string split '.' )
+
+  if test (count $arg_parts) -gt 2 && test $arg_parts[-2] = "tar"
+    set output_dir (string join '.' $arg_parts[1..-3])
+    mkdir $output_dir
+
+    switch $arg_parts[-1]
+      case bz2
+        tar xjf $argv
+      case gz
+        tar xzf $argv
+      case xz
+        tar xf $argv
+      case zst
+        unzstd $argv
+      case *
+        echo "'$argv' cannot be extracted via extractor"
+    end
+  else
+    set output_dir (string join '.' $arg_parts[1..-2])
+    mkdir $output_dir
+
+    switch $arg_parts[-1]
+      case 7z
+        7z x -o$output_dir $argv
+      case zip
+        7z x -o$output_dir $argv
+      case rar
+        7z x -o$output_dir $argv
+      case tbz2
+        tar xjf $argv
+      case bz2
+        bunzip2 $argv
+      case gz
+        gunzip $argv
+      case tar
+        tar xf $argv
+      case tgz
+        tar xzf $argv
+      case Z
+        uncompress $argv;
+      case deb
+        ar x $argv
+      case *
+        echo "'$argv' cannot be extracted via extractor"
+    end
   end
 end
 
